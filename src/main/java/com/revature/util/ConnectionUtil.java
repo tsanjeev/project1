044bc226fd489e9e3e40ab5e1e6e15d69f433d55
Ted Sanjeevi <tsanjeev03@gmail.com>
2018-09-27 14:04:07 -0400

    fix

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -6,15 +6,6 @@ import java.sql.SQLException;
 
 public class ConnectionUtil {
 	public static Connection getConnection() throws SQLException {
-		String url = "jdbc:postgresql://mydb.cd2berwkughw.us-east-2.rds.amazonaws.com:5432/myDB?";
-		String user = "tsanjeev";
-		String password = "revatureFullStax87";
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return DriverManager.getConnection(url, user, password);
+		
 	}
 }
